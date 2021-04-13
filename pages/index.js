69d2462f433f88ed6ca8597b1065271e8a63f2b9
Thomas17101 <56535089+Thomas17101@users.noreply.github.com>
2021-04-13 09:28:50 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,21 +12,9 @@ export default function Home() {
         <h1 className="title">
           Salut
         </h1>
-          class ShoppingList extends React.Component {
-            render() {
-              return (
-                <div className="shopping-list">
-                  <h1>Liste de courses pour {this.props.name}</h1>
-                  <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                  </ul>
-                </div>
-              );
-            }
-          }
-      <ShoppingList name="Marc" />
+        <p className="soustitre">
+          yo
+        </p>
       </main>
 
       <footer>
