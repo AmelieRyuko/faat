00289db0114c7185d3e30e84e035f62a3bb5f6d8
Thomas17101 <56535089+Thomas17101@users.noreply.github.com>
2021-05-10 08:32:24 +0200

    Update unsplash.js

--- pages/unsplash.js
+++ pages/unsplash.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
-import "./styles.css";
+
 
 function App() {
   const [photo, setPhoto] = useState("");
