c24a29fe7d66a6e6f12a4929be431eef14cd72b3
FlorianCo <49975737+FlorianCo@users.noreply.github.com>
2021-05-10 09:36:57 +0200

    Update Citation.js

--- components/Citation.js
+++ components/Citation.js
@@ -4,10 +4,10 @@ class Citation extends React.Component {
   render() {
     return (
         <div id="QuoteOFDay"></div>
-  <script>
+
 var type="theme";
 var theme="ambition";
-  </script>
+
 <script src="https://citations.ouest-france.fr/js/web/export.js?v3"></script> 
     );
   }
