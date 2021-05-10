4ce645550a98a311fe136b41e8ea905afc5052f3
FlorianCo <49975737+FlorianCo@users.noreply.github.com>
2021-05-10 09:04:50 +0200

    Create Citation.js

---
+++ components/Citation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Citation extends React.Component {
+  render() {
+    return (
+        <div id="QuoteOFDay"></div>
+  <script>
+var type="theme";
+var theme="ambition";
+  </script>
+<script src="https://citations.ouest-france.fr/js/web/export.js?v3"></script> 
+    );
+  }
+}
+
+export default Citation
