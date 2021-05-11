001b455ee923db9056512aba2e86edc1a9064e6a
FlorianCo <49975737+FlorianCo@users.noreply.github.com>
2021-05-11 08:49:24 +0200

    Update Citation.js

--- components/Citation.js
+++ components/Citation.js
@@ -4,7 +4,7 @@ import "https://citations.ouest-france.fr/js/web/export.js?v3"
 class Citation extends React.Component {
   render() {
     return (
-        <div id="QuoteOFDay"></div>
+        <div id="QuoteOFDay" /;
 
       var type="theme";
     var theme="ambition";
