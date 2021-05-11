a63d13b9447411bd302a03ae45e03eeae9771e33
Ryuko04 <amelie.reinbold04@gmail.com>
2021-05-11 09:09:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
-import Style from '../styles/style.css'
 
 const Home = () => (
   <div>
     <Head>
-      <title>Connexion</title>
+      <title>Se connecter</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="../styles/style.css">
-  
-  
     </Head>
 
     <Nav />
@@ -18,37 +14,21 @@ const Home = () => (
       <h1 className="title">FAAT</h1>
       </div>
       <form action='home' id='formconnect'>
-            <label>
-             Nom :
-            <input type="text" name="name" required/>
-           </label>
-            <label>
-             prenom :
-            <input type="text" name="firstname" required/>
-           </label>
              <label>
-             mail :
-            <input type="mail" name="mail" required/>
+            Pseudonyme:
+            <input type="pseudo" name="pseudo" required/>
            </label>
             <label>
              Mot de passe :
             <input type="password" name="password" required/>
            </label>
-          <input type="submit" value="Envoyer" />
+          <input type="submit" value="Se connecter" />
     </form>
-  
-  <style jsx>
-  
-      label.jsx-3086735343>input.jsx-3086735343{
-        width: 210px;
-        height: 22px;
-        }
-
+     <style jsx>{`
       .hero {
         width: 100%;
         color: #333;
       }
-
       .title {
         margin: 0;
         width: 100%;
@@ -56,24 +36,19 @@ const Home = () => (
         line-height: 1.15;
         font-size: 48px;
       }
-
       .title,
       .description {
         text-align: center;
       }
-
       #formconnect{
       width : 80%;
       margin : auto;     
       }
-
       #formconnect input{
         display : block;
       width : 100%
       }
-  
-  </style>
-  
+    `}</style>
   </div>
 )
 
