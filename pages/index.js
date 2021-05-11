aa20ef9a143cb27b9afdf4ad27d6391c2be0e593
Ryuko04 <amelie.reinbold04@gmail.com>
2021-05-11 09:40:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ const Home = () => (
     <Head>
       <title>Se connecter</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://github.com/AmelieRyuko/faat/blob/main/public/style.css">
+//       <link rel="stylesheet" href="https://github.com/AmelieRyuko/faat/blob/main/public/style.css">
+        <link rel="stylesheet" href="../public/style.css">
     </Head>
 
     <Nav />
