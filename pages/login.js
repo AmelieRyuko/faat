0773f7b8bf3f1293a536bf43d26e2cfa846429d7
Ryuko04 <amelie.reinbold04@gmail.com>
2021-05-25 18:02:06 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -56,6 +56,16 @@ const Home = () => (
           line-height: 25px;
        }
 
+      #formconnect input:last-child{
+        width: fit-content;
+        margin: auto;
+        padding: 13px;
+        border: none;
+        border-radius: 5px;
+        box-shadow: 3px 3px 7px #80808082;
+        cursor: pointer;
+      }
+
       #formconnect.jsx-2367194551{
         width : 35%;
         margin : auto; 
