a757327b429ff2fa95d76d6ea4984d14022ae2fe
Benoit Maigret <dev@benoitmaigret.com>
2021-05-31 16:29:01 +0200

    Demo with changeMood method

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,48 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+
+import Main from '../components/Main'
 
 const Home = () => (
   <div>
-   <Head>
-      <title>Home</title>
-     <link rel="icon" href="/favicon.ico" />
-   </Head>
+  <Head>
+    <title>Home</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
 
-    <Nav />
-    <div className="hero">
-      <h1 className="title">FAAT</h1>
-      </div>
-      <form action='home' id='formconnect'>
-             <label>
-            Pseudonyme:
-            <input type="pseudo" name="pseudo" required/>
-           </label>
-            <label>
-             Mot de passe :
-            <input type="password" name="password" required/>
-           </label>
-          <input type="submit" value="Se connecter" />
-    </form>
-     <style jsx>{`
-     body{
-     background-color:pink;
-     }
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      #formconnect{
+  <div className="hero">
+    <h1 className="title">FAAT</h1>
+    <Main />
+  </div>
+     
+  <style jsx>{`
+    body{
+      background-color:pink;
+    }
+    .hero {
+      width: 100%;
+      color: #333;
+    }
+    .title {
+      margin: 0;
+      width: 100%;
+      padding-top: 80px;
+      line-height: 1.15;
+      font-size: 48px;
+    }
+    .title,
+    .description {
+      text-align: center;
+    }
+    #formconnect{
       width : 80%;
       margin : auto;     
-      }
-      #formconnect input{
-        display : block;
+    }
+    #formconnect input{
+      display : block;
       width : 100%
-      }
-    `}</style>
+    }
+  `}</style>
   </div>
 )
 
