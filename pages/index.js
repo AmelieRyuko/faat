75a75124a03f29d58ee01cbbae379a58717cb27d
Sani14 <49979819+Sani14@users.noreply.github.com>
2021-06-07 09:46:21 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Main from '../components/Main'
+
+const Home = () => (
+  <div>
+  <Head>
+    <title>Home</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+
+  <div className="hero">
+    <h1 className="title">FAAT</h1>
+    <Main />
+  </div>
+     
+  <style jsx>{`
+    body{
+      background-color:pink;
+    }
+    .hero {
+      width: 100%;
+      color: #333;
+    }
+    .title {
+      margin: 0;
+      width: 100%;
+      padding-top: 80px;
+      line-height: 1.15;
+      font-size: 48px;
+    }
+    .title,
+    .description {
+      text-align: center;
+    }
+    #formconnect{
+      width : 80%;
+      margin : auto;     
+    }
+    #formconnect input{
+      display : block;
+      width : 100%
+    }
+    img{
+      width:100px;
+      weight:100px;
+    }
+  `}</style>
+  </div>
+)
+
+export default Home
