11b7bc5b6fcfe3cc5aa74494be7459faed76bd51
Sani14 <49979819+Sani14@users.noreply.github.com>
2021-06-07 09:54:52 +0200

    Update mycomponent.js

--- components/mycomponent.js
+++ components/mycomponent.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Button from '../components/Button.js'
-//import axios from "axios";
+
 
 class MyComponent extends React.Component {
     constructor(props) {
